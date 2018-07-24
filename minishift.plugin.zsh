63971821918773065f4f8d3bbc5e775490fc476a
Philipp Born <philipp@tamcore.eu>
2018-07-24 09:17:27 +0200

    check status of minishift instance before executing oc-env / docker-env commands

--- minishift.plugin.zsh
+++ minishift.plugin.zsh
@@ -1,3 +1,9 @@
-eval "$(minishift oc-env)"
-eval "$(minishift docker-env)"
+ZSH_MINISHIFT_STATUS=$( minishift status | awk '$1=="Minishift:" {print $2}' )
+
+if [ "${ZSH_MINISHIFT_STATUS}" = "Running" ]
+then
+  eval "$(minishift oc-env)"
+  eval "$(minishift docker-env)"
+fi
+
 eval "$(oc completion zsh)"
